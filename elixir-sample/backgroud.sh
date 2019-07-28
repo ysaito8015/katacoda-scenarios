4cd91ad6746c00626c60d137ec4650123c8939ff
Yusuke_Saito <ysaito8015@gmail.com>
2019-07-28 20:49:07 +0900

    add sample codes

---
+++ elixir-sample/backgroud.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+apt update && apt dist-upgrade -y
+
+apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt update && apt install -y docker-ce
+
+curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+
---
+++ elixir-sample/step1.md
@@ -0,0 +1,21 @@
+elixir/phoenix ハンズオンのサンプルです
+
+## 環境
+
+```
+Ubuntu 19.04
+Docker
+Docker-compose
+```
+
+## 課題
+
+次のコマンドを右のターミナルで実行してください:
+
+`git clone https://github.com/kuroda/live_dendron.git live_dendron`{{execute}}
+
+リポジトリ内へ移動し、 `install.sh` を実行します
+
+`cd ./live_dendron`{{execute}}
+
+`./install.sh`{{execute}}
