962018aa9025ec3bfc9008ba5b971eee994ef89e
Yusuke_Saito <ysaito8015@gmail.com>
2019-09-01 21:52:19 +0200

    amend Dockerfile

--- elixir-phoenix/elixir-phoenix-first-02/setup.sh
+++ elixir-phoenix/elixir-phoenix-first-02/setup.sh
@@ -2,6 +2,22 @@
 
 mkdir -p /elixir; cd /elixir/
 
+cat - << EOS > ./Dockerfile
+FROM elixir:1.3.4
+
+ENV NVMURL https://rawgit.com/creationix/nvm/v0.33.6/install.sh
+
+ENV TEMP =https://github.com/phoenixframework/archives/raw/master
+
+RUN cd /tmp && wget --no-check-certificate $NVMURL && bash ./install.sh && rm -f ./install.sh && source ~/.bashrc
+
+RUN nvm install 8.9
+
+RUN mix local.hex --force && mix local.raber && mix archive.install $TEMP/phoenix_new-1.2.5.ez --force
+
+WORKDIR /app
+EOS
+
 docker build -t modest_greeter:1.2.5 .
 
 docker run -it modest_greeter:1.2.5 /bin/bash
