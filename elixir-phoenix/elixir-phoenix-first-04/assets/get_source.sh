5fdc8c83a9ed31dcba3bcf9b82b809c3e793b167
Yusuke_Saito <ysaito8015@gmail.com>
2019-09-03 08:49:15 +0200

    add assets

---
+++ elixir-phoenix/elixir-phoenix-first-04/assets/get_source.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cd /root/oiax/projects/
+
+wget -Oq - https://github.com/ysaito8015/modest_greeter/archive/ch05.tar.gz | tar --transform 's/modest_greeter-ch[0-9][0-9]/modest_greeter/' -zx
+
+cd /root/oiax/projects/modest_greeter/
