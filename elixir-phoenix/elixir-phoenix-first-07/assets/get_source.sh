1b54080186f1f3ec4ef8717875d629bb18b2bba2
Yusuke_Saito <ysaito8015@gmail.com>
2019-09-07 14:20:05 +0200

    amend get_source.sh

--- elixir-phoenix/elixir-phoenix-first-07/assets/get_source.sh
+++ elixir-phoenix/elixir-phoenix-first-07/assets/get_source.sh
@@ -5,5 +5,4 @@ cd /root/oiax/projects/
 wget -qO- https://github.com/ysaito8015/modest_greeter/archive/ch11.tar.gz | tar --transform 's/modest_greeter-ch[0-9][0-9]/modest_greeter/' -zx \
   && cd /root/oiax/projects/modest_greeter/ \
   && mix deps.get \
-  && npm install \
-  && mix compile
+  && npm install
