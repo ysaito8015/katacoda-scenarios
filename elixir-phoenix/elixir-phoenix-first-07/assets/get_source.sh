c504b0d97579012c0329fc661dd757b8a6051ba1
Yusuke_Saito <ysaito8015@gmail.com>
2019-09-07 18:31:39 +0200

    amend npm isntall

--- elixir-phoenix/elixir-phoenix-first-07/assets/get_source.sh
+++ elixir-phoenix/elixir-phoenix-first-07/assets/get_source.sh
@@ -5,4 +5,4 @@ cd /root/oiax/projects/
 wget -qO- https://github.com/ysaito8015/modest_greeter/archive/ch11.tar.gz | tar --transform 's/modest_greeter-ch[0-9][0-9]/modest_greeter/' -zx \
   && cd /root/oiax/projects/modest_greeter/ \
   && mix deps.get \
-  && npm install
+  && npm install && node node_modules/brunch/bin/brunch build
