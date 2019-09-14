19b4a0d98ea517bde84b4fdb73c9aeb7f5205825
Yusuke_Saito <ysaito8015@gmail.com>
2019-09-14 22:41:10 +0200

    change backgroud job

--- elixir-getting-started/elixir-getting-started-01/setup.sh
+++ elixir-getting-started/elixir-getting-started-01/setup.sh
@@ -5,4 +5,4 @@ mkdir -p /work; cd $_
 export CODER_URL=" https://github.com/cdr/code-server/releases/download/1.1156-vsc1.33.1/code-server1.1156-vsc1.33.1-linux-x64.tar.gz"
 wget -qO- $CODER_URL | tar --transform 's/code-server1.1156-vsc1.33.1-linux-x64/code-server/g' -zxv -C /opt
 
-/opt/code-server/code-server -N -p 23000
+/opt/code-server/code-server -N -p 23000 &
