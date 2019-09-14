fe65530801145b20a1b7e16ffb0ae099df2dc742
Yusuke_Saito <ysaito8015@gmail.com>
2019-09-14 22:23:27 +0200

    add scenario elixir-getting-started

---
+++ elixir-getting-started/elixir-getting-started-01/intro.md
@@ -0,0 +1,5 @@
+## 概要
+
+piacerex さんの Qiita 記事 
+[Excelから関数型言語マスター1回目：データ行の”並べ替え”と”絞り込み”](https://qiita.com/piacerex/items/6714e1440e3f25fb46a1)
+を元にして, elixir を体験するシナリオです
---
+++ elixir-getting-started/elixir-getting-started-01/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p /work; cd $_
+
+export CODER_URL=" https://github.com/cdr/code-server/releases/download/1.1156-vsc1.33.1/code-server1.1156-vsc1.33.1-linux-x64.tar.gz"
+wget -qO- $CODER_URL | tar --transform 's/code-server1.1156-vsc1.33.1-linux.x64.tar.gz/code-server/g' -zxv -C /opt
+
+/opt/code-server/code-server -N -p 23000
