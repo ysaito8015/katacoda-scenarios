1cce6d261fe0f4c8d7acb2511dd33c834ee89d04
Yusuke_Saito <ysaito8015@gmail.com>
2019-09-14 22:27:19 +0200

    amend typo

--- elixir-getting-started/elixir-getting-started-01/setup.sh
+++ elixir-getting-started/elixir-getting-started-01/setup.sh
@@ -3,6 +3,6 @@
 mkdir -p /work; cd $_
 
 export CODER_URL=" https://github.com/cdr/code-server/releases/download/1.1156-vsc1.33.1/code-server1.1156-vsc1.33.1-linux-x64.tar.gz"
-wget -qO- $CODER_URL | tar --transform 's/code-server1.1156-vsc1.33.1-linux.x64.tar.gz/code-server/g' -zxv -C /opt
+wget -qO- $CODER_URL | tar --transform 's/code-server1.1156-vsc1.33.1-linux-x64.tar.gz/code-server/g' -zxv -C /opt
 
 /opt/code-server/code-server -N -p 23000
