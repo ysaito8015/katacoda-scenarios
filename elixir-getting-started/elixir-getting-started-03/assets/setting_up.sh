fdc6550ebce350e416b4b724db5fa7b8b471cd64
Yusuke_Saito <ysaito8015@gmail.com>
2019-09-15 20:55:12 +0200

    amend intro.md, setting_up.sh

--- elixir-getting-started/elixir-getting-started-03/assets/setting_up.sh
+++ elixir-getting-started/elixir-getting-started-03/assets/setting_up.sh
@@ -3,5 +3,5 @@
 apt update && apt install -y inotify-tools sudo \
 && wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash \
 && source ~/.bashrc \
-&& nvm install \
+&& nvm install node\
 && mix local.hex --force && mix local.rebar --force && mix archive.install phx_new --force
--- elixir-getting-started/elixir-getting-started-03/intro.md
+++ elixir-getting-started/elixir-getting-started-03/intro.md
@@ -1,5 +1,5 @@
 ## 概要
 
 piacerex さんの Qiita 記事 
-[Excelから関数型言語マスター2回目：データ列の”抽出”、”Web表示”](https://qiita.com/piacerex/items/b7787580fce5f148242f)
+[Excelから関数型言語マスター3回目：WebにDBデータ表示【PostgreSQL or MySQL編】](https://qiita.com/piacerex/items/a7558adc6856e3577dc6)
 を元にして, elixir を体験するシナリオです
