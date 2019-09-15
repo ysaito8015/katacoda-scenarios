1ddac5f3ea38bb20f4451783a83120eabe336c12
Yusuke_Saito <ysaito8015@gmail.com>
2019-09-15 21:01:39 +0200

    amend setting_up

--- elixir-getting-started/elixir-getting-started-03/assets/setting_up.sh
+++ elixir-getting-started/elixir-getting-started-03/assets/setting_up.sh
@@ -4,4 +4,4 @@ apt update && apt install -y inotify-tools sudo \
 && wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash \
 && source ~/.bashrc \
 && nvm install node\
-&& mix local.hex --force && mix local.rebar --force && mix archive.install phx_new --force
+&& mix local.hex --force && mix local.rebar --force && mix archive.install hex phx_new --force
