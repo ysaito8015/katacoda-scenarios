7f0953da1b5c8dc5875f2e2322e77c138275cc71
Yusuke_Saito <ysaito8015@gmail.com>
2019-09-26 23:51:53 +0200

    amend get_source

--- webpack-tutorials/webpack-tutorial-02/assets/get_source.sh
+++ webpack-tutorials/webpack-tutorial-02/assets/get_source.sh
@@ -6,7 +6,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash 
   && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" \
   && [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" \
   && nvm install --lts && nvm alias default lts/* \
-  && mkdir /work && cd /work \
+  && mkdir -p /work && cd /work \
   && wget -qO- https://github.com/ysaito8015/webpack-demo/archive/getting_started.tar.gz | tar --transform 's/webpack-demo-getting_started/webpack-demo/' -zx \
   && npm update \
   && sed -i.bak -e 's/\/var\/www\/>/\/work\/webpack-demo>/g' /etc/apache2/apache2.conf \
