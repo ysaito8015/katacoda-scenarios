79ba11c7626b2e74500c6a079285322244971417
Yusuke_Saito <ysaito8015@gmail.com>
2019-10-14 01:02:13 +0200

    amend 01/setup

--- packgo-walkthrough/packgo-walkthrough-01/setup.sh
+++ packgo-walkthrough/packgo-walkthrough-01/setup.sh
@@ -2,6 +2,8 @@
 
 mkdir -p /work && cd $_
 
-wget -qO- "https://dl.google.com/go/go1.13.1.linux-amd64.tar.gz" | tar -zxv -C /opt
+wget  "https://dl.google.com/go/go1.13.1.linux-amd64.tar.gz"
+tar zxvf go1.13.1.linux-amd64.tar.gz -C /opt
 ln -s /opt/go/bin/go /usr/local/bin/go
 ln -s /opt/go/bin/gofmt /usr/local/bin/gofmt
+
