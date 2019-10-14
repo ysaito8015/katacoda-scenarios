4bf8871597296e015831c8b511249885d0d35721
Yusuke_Saito <ysaito8015@gmail.com>
2019-10-14 09:48:17 +0200

    amend setup

--- pacgo-walkthrough/pacgo-walkthrough-08/setup.sh
+++ pacgo-walkthrough/pacgo-walkthrough-08/setup.sh
@@ -10,4 +10,6 @@ ln -s /opt/go/bin/gofmt /usr/local/bin/gofmt
 
 wget -O- https://raw.githubusercontent.com/danicat/pacgo/master/step08/maze01.txt > /work/pacgo/maze01.txt
 wget -O- https://raw.githubusercontent.com/danicat/pacgo/master/step07/main.go > /work/pacgo/main.go
+wget -O- https://raw.githubusercontent.com/danicat/pacgo/master/step08/config.json > /work/pacgo/config.json
+wget -O- https://raw.githubusercontent.com/danicat/pacgo/master/step08/config_noemoji.json > /work/pacgo/config_noemoji.json
 
