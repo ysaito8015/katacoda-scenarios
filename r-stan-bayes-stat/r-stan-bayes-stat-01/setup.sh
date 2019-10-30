e20f71ea003d8b0d0757f5cc97733b70d7f8a10a
Yusuke_Saito <ysaito8015@gmail.com>
2019-10-30 18:30:11 +0100

    amend docker image

--- r-stan-bayes-stat/r-stan-bayes-stat-01/setup.sh
+++ r-stan-bayes-stat/r-stan-bayes-stat-01/setup.sh
@@ -2,6 +2,6 @@
 
 mkdir -p /work && cd /work
 
-docker pull data-refine/stan-d:latest
+docker pull datarefine/paper-r-dev:latest
 
-docker run -it -e PASSWORD=rstanrocker -p 8787:8787 data-refine/stan-d:latest /bin/bash
+docker run -it -e PASSWORD=password -p 8787:8787 -v ~:/home/rstudio -d --name name_of_container data-refine/paper-r-dev:latest
