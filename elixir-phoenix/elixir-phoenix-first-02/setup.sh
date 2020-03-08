9d49e1e5e3ff476b3eee562e01cd132acd2159d9
Yusuke_Saito <ysaito8015@gmail.com>
2020-03-08 07:25:52 +0100

    update

--- elixir-phoenix/elixir-phoenix-first-02/setup.sh
+++ elixir-phoenix/elixir-phoenix-first-02/setup.sh
@@ -1,29 +1,23 @@
 #!/bin/bash
 
-mkdir -p /root/oiax/elixir-primer/v01/ch03; cd /root/oiax
+mkdir -p /work/projects/elixir-primer/v01/ch03; cd /work/projects
 
 cat << 'EOS' > ./Dockerfile
-FROM elixir:1.3.4
+FROM elixir:1.9.1
 
 SHELL ["/bin/bash", "-c"]
 
-ENV NVMURL https://rawgit.com/creationix/nvm/v0.33.6/install.sh
-
-ENV TEMP https://github.com/phoenixframework/archives/raw/master
-
 RUN touch ~/.bashrc && cd /tmp \
-&& wget --no-check-certificate $NVMURL \
-&& bash ./install.sh \
-&& rm -f ./install.sh \
+&& wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash \
 && source ~/.bashrc \
-&& nvm install 8.9 \
+&& nvm install \
 && apt update \
 && apt install -y inotify-tools
 
-RUN mix local.hex --force && mix local.rebar && mix archive.install $TEMP/phoenix_new-1.2.5.ez --force
+RUN mix local.hex --force && mix local.rebar --force && mix archive.install phx_new --force
 
 EOS
 
-docker build -t modest_greeter:1.2.5 .
+docker build -t modest_greeter:1.4.10 .
 
-docker run -it -v /root/oiax:/root/oiax -p 4000:4000 modest_greeter:1.2.5 /bin/bash
+docker run -it -v /work/projects:/work/projects -p 4000:4000 modest_greeter:1.4.10 /bin/bash
