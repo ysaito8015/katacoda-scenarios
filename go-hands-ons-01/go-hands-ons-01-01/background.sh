2a3600c2bfd3914338f5795eb65943aa5e140711
Yusuke_Saito <ysaito8015@gmail.com>
2020-03-08 08:03:10 +0100

    update

--- go-hands-ons-01/go-hands-ons-01-01/background.sh
+++ go-hands-ons-01/go-hands-ons-01-01/background.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-sudo apt-get update && sudo apt-get upgrade
-
 export CODER_URL="https://github.com/cdr/code-server/releases/download/3.0.0/code-server-3.0.0-linux-x86_64.tar.gz" \
  && wget -qO- $CODER_URL | tar --transform 's/code-server-3.0.0-linux-x86_64/code-server/' -zxv -C /opt \
  && /opt/code-server/code-server -N --port 23000 &
--- go-hands-ons-01/go-hands-ons-01-01/setup.sh
+++ go-hands-ons-01/go-hands-ons-01-01/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 mkdir -p /work; cd $_
 
-wget  "https://dl.google.com/go/go1.14.linux-amd64.tar.gz"
-tar zxvf go1.14.linux-amd64.tar.gz -C /opt
-ln -s /opt/go/bin/go /usr/local/bin/go
-ln -s /opt/go/bin/gofmt /usr/local/bin/gofmt
+wget -qO- "https://dl.google.com/go/go1.14.linux-amd64.tar.gz" | tar -zxv -C /opt \
+&& ln -s /opt/go/bin/go /usr/local/bin/go \
+&& ln -s /opt/go/bin/gofmt /usr/local/bin/gofmt
