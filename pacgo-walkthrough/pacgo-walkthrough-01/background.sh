91cfa321d9c75052c300433c2fc6a17a1862c2f5
Yusuke_Saito <ysaito8015@gmail.com>
2020-03-08 08:55:08 +0100

    update

--- pacgo-walkthrough/pacgo-walkthrough-01/background.sh
+++ pacgo-walkthrough/pacgo-walkthrough-01/background.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export CODER_URL="https://github.com/cdr/code-server/releases/download/3.0.0/code-server-3.0.0-linux-x86_64.tar.gz" \
- && wget -qO- $CODER_URL | tar --transform 's/code-server-3.0.0-linux-x86_64/code-server/' -zxv -C /opt \
- && /opt/code-server/code-server -N --port 23000 &
+export CODER_URL="https://github.com/cdr/code-server/releases/download/2.1698/code-server2.1698-vsc1.41.1-linux-x86_64.tar.gz" \
+ && wget -qO- $CODER_URL | tar --transform 's/code-server2.1698-vsc1.41.1-linux-x86_64/code-server/' -zxv -C /opt \
+ && /opt/code-server/code-server --auth none --port 23000 &
