7af7ed6b1f821ad779046c8b29e0eb6534d56a4b
Yusuke Saito <ysaito8015@gmail.com>
2021-12-02 15:41:17 +0100

    update

---
+++ getting-started-with-git/getting-started-with-git-01/setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+mkdir -p /work && chown -R packer:packer /work
+
+su - packer && cd /work
+
+sudo docker pull datarefine/rstudio:latest
+
+sudo docker run -it -e DISABLE_AUTH=true -p 8787:8787 -v /work:/home/rstudio/work -d --name rstudio datarefine/rstudio:latest
