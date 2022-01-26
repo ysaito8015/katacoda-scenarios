8dfd2181a80c15959a76350ca8136666491e46f5
Yusuke Saito <ysaito8015@gmail.com>
2022-01-26 22:38:40 +0100

    update

--- elixir-getting-started/elixir-getting-started-02/setup.sh
+++ elixir-getting-started/elixir-getting-started-02/setup.sh
@@ -2,6 +2,6 @@
 
 mkdir -p /work; cd $_
 
-docker pull elixir:1.9.1
+docker pull elixir:1.13.2
 
-docker run -it -v /work:/work -p 4000:4000 elixir:1.9.1 /bin/bash
+docker run -it -v /work:/work -p 4000:4000 elixir:1.13.2 /bin/bash
