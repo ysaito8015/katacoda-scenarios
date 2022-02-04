b8206a6125d0239b80d3358ecbc84dbf53d844dc
Yusuke Saito <ysaito8015@gmail.com>
2022-02-04 04:11:15 +0100

    update

--- postgresql-tutorials/02_where/setup.sh
+++ postgresql-tutorials/02_where/setup.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-apt install -y postgresql-common && sh /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh && apt update && apt install -y postgresql-14 &
-wait
+command1() {
+	apt install -y postgresql-common
+	sh /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
+	apt update && apt install -y postgresql-14
+}
+
+command1 &
+PID=$!
+wait $PID
 su - postgres
 createdb dvdrental
 pg_restore -U postgres -d dvdrental /work/dvdrental.tar
